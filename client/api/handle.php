0bdbe6ef7457976961e3d8aec16231cd546da992
Alexandr <82879664+DrobotAlexandr@users.noreply.github.com>
2021-11-20 19:23:19 +0300

    Update handle.php

--- client/api/handle.php
+++ client/api/handle.php
@@ -8,20 +8,24 @@ include '../../src/ImageNormalize.php';
 
 include 'size.php';
 
-$task = include 'tmp/queue/' . ($_GET['id']) . '.php';
+$error = false;
 
-$noHandle = false;
+try {
 
-if ($config['exceptionMasks']) {
-    foreach ($config['exceptionMasks'] as $exception) {
-        if (strstr($task['task'], $exception)) {
-            $noHandle = true;
+    $task = include 'tmp/queue/' . ($_GET['id']) . '.php';
+
+    $noHandle = false;
+
+    if ($config['exceptionMasks']) {
+        foreach ($config['exceptionMasks'] as $exception) {
+            if (strstr($task['task'], $exception)) {
+                $noHandle = true;
+            }
         }
     }
-}
 
-if (!$noHandle) {
-    if (file_exists($task['task'])) {
+    if (!$noHandle) {
+
         $res = ImageNormalize::normalize(
             [
                 'src' => $task['task'],
@@ -29,27 +33,33 @@ if (!$noHandle) {
             ]
         );
     }
-}
 
-$size = false;
+    $size = false;
 
-if ($_GET['getSize']) {
+    if ($_GET['getSize']) {
 
-    $size = getSize(
-        [
-            'path' => $config['folder']
-        ]
-    );
+        $size = getSize(
+            [
+                'path' => $config['folder']
+            ]
+        );
 
-}
+    }
 
-if ($size['size']) {
-    $size['size'] = round($size['size'] / 1000000);
+    if ($size['size']) {
+        $size['size'] = round($size['size'] / 1000000);
+    }
+
+} catch (Throwable  $t) {
+
+    $error = $t->getMessage()
+
 }
 
 $res = [
     'status' => 'ok',
     'file' => $res['src']['local'],
+    'error' => $error,
     'size' => $size
 ];
 
